247226881d5eeba7d771d30e484db9fe8cd6f0eb
Jan Noel <noel.calayag@swapoolabs.com>
2020-12-18 17:28:08 +0800

    Create template for daily challenges

---
+++ dayXX.py
@@ -0,0 +1,14 @@
+def find_answer_1(input):
+    return
+
+
+def find_answer_2(input):
+    return
+
+
+if __name__ == "__main__":
+    with open("dayXX.txt") as f:
+        input = [str(x) for x in f.read().splitlines()]
+
+    print(find_answer_1(input))
+    print(find_answer_2(input))
